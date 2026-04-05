de5cf66fef54b3ebea003289003c9d24847a7da8
pritam <pritamprajapati666@gmail.com>
2026-04-05 19:33:58 +0530

    adding user model

--- src/model/User.ts
+++ src/model/User.ts
@@ -65,3 +65,10 @@ const userSchema : Schema<User> = new Schema({
     },
     messages: [messageSchema]
 })
+
+
+const UserModel =
+  (mongoose.models.User as mongoose.Model<User>) ||
+  mongoose.model<User>('User', userSchema);
+
+export default UserModel;
